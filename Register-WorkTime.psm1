3e8702a97eebaac1721dcf0acd9c5d51064e50bf
Rafael Moreno Romão <rafaelm@takenet.com.br>
2016-08-01 10:24:42 -0300

    get projects from toggl to create tasks for wakatime durations

--- Register-WorkTime.psm1
+++ Register-WorkTime.psm1
@@ -20,6 +20,7 @@ function ParseArguments($input_args) {
 		$result | Add-Member -type NoteProperty -name printHelp -value $false
         $result | Add-Member -type NoteProperty -name wakatimeApiKey -value $null
         $result | Add-Member -type NoteProperty -name togglApiToken -value $null
+        $result | Add-Member -type NoteProperty -name togglWorkspace -value $null
         $result | Add-Member -type NoteProperty -name clockFile -value $null
         $result | Add-Member -type NoteProperty -name date -value $evaluationDate
         $result | Add-Member -type NoteProperty -name fileName -value $fileName
@@ -50,6 +51,10 @@ function ParseArguments($input_args) {
 			$result.togglApiToken = "$($nextArg)"
 		}
 
+		if ($arg -eq "--togglWorkspace" -or $arg -eq "-s") {
+			$result.togglWorkspace = "$($nextArg)"
+		}
+
 		if ($arg -eq "--clockFile" -or $arg -eq "-c") {
 			$result.clockFile = "$($nextArg)"
 		}
@@ -67,8 +72,9 @@ function CheckIfMustPrintHelp($printHelp) {
 	if ($printHelp) {
 		Write-Host ""
 		Write-Host "--help `t`t`t -h `t Print usage options"
-        Write-Host "--wakatime-apikey `t`t`t -w `t Inform your wakatime api key"
-		Write-Host "--toggl-apitoken `t`t -t `t Inform your toggl api token"
+        Write-Host "--wakatimeApiKey `t`t`t -w `t Inform your wakatime api key"
+		Write-Host "--togglApiToken `t`t -t `t Inform your toggl api token"
+        Write-Host "--togglWorkspace `t`t -s `t Inform the workspace to save the projects on toggl"
 		Write-Host "--clock-file `t`t`t -c `t Inform the path for your time clock excel file"
         Write-Host "--date `t`t`t -d `t Inform the date of evaluation (Default = current day)"
 		Write-Host ""
@@ -105,6 +111,11 @@ function CheckRequestAndStoreMandatoryParameters($arguments) {
 	if ($arguments.togglApiToken -eq $null) {
 		Write-Host 'Informe your Toggl API Token:'
 		$arguments.togglApiToken = Read-Host;
+		$updateFile = $true;
+	}
+	if ($arguments.togglWorkspace -eq $null) {
+		Write-Host 'Inform the workspace to save the projects on toggl:'
+		$arguments.togglWorkspace = Read-Host;
 		$updateFile = $true;
 	}
 	if ($arguments.clockFile -eq $null) {
@@ -150,6 +161,53 @@ function GetWorkTimeEntriesFromWakaTime($arguments) {
 	if ($durations -eq $null) {
 		return $null
 	}
+    $durations = $durations | Select-Object -ExpandProperty data
+	if ($durations -eq $null) {
+		return $null
+	}
+
+    $togglAuth = "${arguments.togglApiToken}:api_token"
+    $workspacesUri = "https://www.toggl.com/api/v8/workspaces"
+    $response = curl -k -v -u $togglAuth -X GET $workspacesUri
+    $workspaces = $response | ConvertFrom-Json 2> $null
+    $workspaceId = $workspaces | Where-Object -Property name -eq ${arguments.togglWorkspace} | Select-Object -ExpandProperty id
+	if ($arguments.debug) {
+		Write-Host "workspaceId: $workspaceId" 
+	}
+
+    $projectUri = "https://www.toggl.com/api/v8/workspaces/$workspaceId/projects"
+    $response = curl -k -v -u $togglAuth -X GET $projectUri
+    $projects = $response | ConvertFrom-Json 2> $null
+    $createProjectUri = "https://www.toggl.com/api/v8/projects"
+
+    for ($i = 0; $i -lt $durations.Length; $i++) {
+        $duration = $durations[$i]
+        $projectName = $duration | Select-Object -ExpandProperty project 
+        $project = $projects | Where-Object -Property name -eq $projectName
+        if ($project -eq $null) {
+           $createProjectPayload = '{"project":{"name":"' + $projectName + '","wid":' + $workspaceId + '}}'
+           if ($arguments.debug) {
+               Write-Host "createProjectPayload: $createProjectPayload" 
+           }
+           $response = curl -k -v -u $togglAuth -H $contentType -d $createProjectPayload -X POST $createProjectUri
+           if ($arguments.debug) {
+               Write-Host "response: $response" 
+           }
+           $project = $response | ConvertFrom-Json
+        } 
+
+        #TODO: Save task for duration
+
+
+
+    }
+
+
+	if ($arguments.debug) {
+		Write-Host "durations: $durations" 
+	}
+
+
 	return $durations
 }
 
